15f7a7de70b71eddca1a636a8b0956a17b38cdc2
AdrianaIaffa <adrianaiaffa@hotmail.com>
2024-04-19 11:39:49 +0100

    add keyword search in website body and html parsing (ref#3)

--- processUrls.js
+++ processUrls.js
@@ -1,6 +1,8 @@
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 const { fetchSiteMap, parseXmlData } = require("./sitemapFetcher");
+const HTMLParser = require('node-html-parser')
+const readlineSync = require('readline-sync')
 
 const sitemapUrl = "https://www.london.gov.uk/sitemap.xml?page=1";
 
@@ -10,23 +12,44 @@ async function processUrls() {
     const xmlData = await fetchSiteMap(sitemapUrl);
     const urlList = await parseXmlData(xmlData);
 
+    const keyword = readlineSync.question('Enter keyword or phrase: ')
+    // const totalMatches = []
     for (const url of urlList) {
+        
       try {
-
+        
         const response = await fetch(url);
         if (!response.ok) {
           console.error(`Error fetching ${url}: ${response.status}`);
           continue;
         }
         const htmlData = await response.text();
-        console.log(htmlData);
-        console.log(`Processed data from ${url}`);
+        // console.log(htmlData);
+       
+       const root = HTMLParser.parse(htmlData)
+       const bodyText = root.querySelector('body').textContent;
 
-      } catch {
+       const keywordRegExp = new RegExp(keyword, 'gi')
+       const matches = bodyText.match(keywordRegExp)
+       
+       if(matches === null) {
+        console.log("no matches")
+       } else {
+        // totalMatches.push(matches.length)
+        console.log(`Keyword: ${keyword} - Times Found: ${matches.length} (on page: ${url})`)
+       }
+       
+
+       console.log(`Processed data from ${url}`);
+    //    console.log(bodyText)
+    
+
+      } catch (error) {
 
         console.error(`Error fetching ${url}: ${error.message}`);
         continue;
       }
+
     }
   } catch (error) {
 
