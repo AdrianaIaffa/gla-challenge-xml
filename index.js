6591bda2a2d4e41d98a09ccce3f7f0881e4de399
AdrianaIaffa <adrianaiaffa@hotmail.com>
2024-04-22 17:56:55 +0100

    added remove unwanted tags when parsing body. also created a folder with the parsed html content to be able to compare if the search is correct. The files are created inside a folder called 'html_files'. also updated json file as added and removed library while testing out a few things (ref #3)

--- index.js
+++ index.js
@@ -4,18 +4,6 @@ const { generateReport } = require('./generateReport');
 
 const sitemapUrl = "https://www.london.gov.uk/sitemap.xml?page=1"
 
-// async function run() {
-//     try {
-//         const xmlData = await fetchSiteMap(sitemapUrl)
-//         const urlList = parseXmlData(xmlData)
-//         console.log(urlList)
-
-//     } catch(error) {
-//         console.error('Error in main:', error)
-//     }
-// }
-
-// run()
 
 async function run() {
     try {
